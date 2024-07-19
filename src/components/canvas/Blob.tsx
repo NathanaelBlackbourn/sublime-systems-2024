79ca52e22711bc6b55ee9ac42a59d6fb51a9a67b
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-07-19 16:29:04 +0200

    Remove onClick from Blob

--- src/components/canvas/Blob.tsx
+++ src/components/canvas/Blob.tsx
@@ -1,18 +1,11 @@
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useCursor, MeshDistortMaterial } from '@react-three/drei';
 
 const Blob = ({ route = '/', ...props }) => {
-  const router = useRouter();
   const [hovered, hover] = useState(false);
   useCursor(hovered);
   return (
-    <mesh
-      onClick={() => router.push(route)}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
-      {...props}
-    >
+    <mesh onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} {...props}>
       <sphereGeometry args={[1, 64, 64]} />
       <MeshDistortMaterial roughness={0.5} color={hovered ? 'hotpink' : '#1fb2f5'} />
     </mesh>
