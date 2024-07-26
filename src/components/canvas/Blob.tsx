4f826473aa9790175fef2c1bc1c67e1ac1ce5bfb
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-07-26 15:56:15 +0200

    Achieve reflection and refraction

--- src/components/canvas/Blob.tsx
+++ src/components/canvas/Blob.tsx
@@ -1,10 +1,23 @@
-import { MeshDistortMaterial } from '@react-three/drei';
+import * as THREE from 'three';
+
+import { useCubeTexture } from '@react-three/drei';
 
 const Blob = ({ route = '/', ...props }) => {
+  const refractionMap = useCubeTexture(new Array(6).fill('panel.png'), {
+    path: 'img/environment/',
+  });
+
+  const reflectionMap = useCubeTexture(new Array(6).fill('panel.png'), {
+    path: 'img/environment/',
+  });
+
+  refractionMap.mapping = THREE.CubeRefractionMapping;
+  reflectionMap.mapping = THREE.CubeReflectionMapping;
+
   return (
     <mesh {...props}>
       <sphereGeometry args={[1, 64, 64]} />
-      <MeshDistortMaterial roughness={0.7} color={'#001aff'} />
+      <meshPhongMaterial color={'0xccfffd'} envMap={refractionMap} refractionRatio={0.985} reflectivity={1} />
     </mesh>
   );
 };
