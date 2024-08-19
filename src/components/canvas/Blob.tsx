41757a7f2972b381d7d976b1ac0629e589150135
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-08-19 20:17:13 +0200

    Change to distort material

--- src/components/canvas/Blob.tsx
+++ src/components/canvas/Blob.tsx
@@ -1,8 +1,10 @@
+import { MeshDistortMaterial } from '@react-three/drei';
+
 const Blob = ({ route = '/', ...props }) => {
   return (
     <mesh {...props}>
       <sphereGeometry args={[1, 64, 64]} />
-      <meshPhysicalMaterial color={'white'} roughness={0.1} metalness={0.0} transmission={1.0} thickness={1.0} />
+      <MeshDistortMaterial color={'white'} distort={0.4} />
     </mesh>
   );
 };
