4878d249a3d1209336ee5e884bb87e274b232d8f
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-08-20 11:31:01 +0200

    Change blob color

--- src/components/canvas/Blob.tsx
+++ src/components/canvas/Blob.tsx
@@ -4,7 +4,7 @@ const Blob = ({ route = '/', ...props }) => {
   return (
     <mesh {...props}>
       <sphereGeometry args={[1, 64, 64]} />
-      <MeshDistortMaterial color={'white'} distort={0.4} />
+      <MeshDistortMaterial color={'#1001d5'} distort={0.4} />
     </mesh>
   );
 };
