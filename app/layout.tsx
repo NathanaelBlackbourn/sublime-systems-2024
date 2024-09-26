22af774060026442a9fd0bd2e61296b9944e30a2
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-09-26 11:17:06 +0200

    Install Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import { Layout } from '@/components/dom/Layout/Layout';
 import '@/global.css';
+import localFont from 'next/font/local';
+
+const inter = localFont({
+  src: './fonts/InterVariable.woff2',
+  weight: '100-900',
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'SUBLIME SYSTEMS',
@@ -8,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.className}>
       <head />
       <body>
         <Layout>{children}</Layout>
