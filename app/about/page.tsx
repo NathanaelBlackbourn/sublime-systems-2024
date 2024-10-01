db0d97afae043a445cd317479a9f111e22157748
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-10-01 09:39:47 +0200

    Build experience block

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,11 +2,17 @@ import ImageLoader from '@/components/dom/ImageLoader/ImageLoader';
 import classes from './page.module.scss';
 import Content from '@/components/dom/Content/Content';
 import HeroBlock from '@/components/dom/About/HeroBlock/HeroBlock';
+import experience from '@/data/experience';
+import ExperienceBlock from '@/components/dom/About/ExperienceBlock/ExperienceBlock';
 
 const Page = () => {
   return (
     <Content>
       <HeroBlock />
+      <h2 className={classes['heading']}>Experience</h2>
+      {experience.map((item, i) => (
+        <ExperienceBlock key={i} {...item} />
+      ))}
     </Content>
   );
 };
